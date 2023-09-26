742278f4a9f877a14f9860df3f435013da081705
Konstantins-Cmils <konstantins.cmils@gmail.com>
2023-09-26 23:15:05 +0300

    Pievienots sākuma skats

---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layout')
+
+@section('content')
+
+ <h1>PD 2 - {{ $title }}</h1>
+ 
+@endsection
---
+++ src/resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="lv">
+ <head>
+ <meta charset="utf-8">
+ <title>PD 2 - {{ $title }}</title>
+ <meta name="description" content="Timekēkļa tehnoloģijas 2. praktiskais darbs">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ </head>
+ <body>
+ @yield('content') <!-- blade šabloan dzinēju funkcija;-->
+ </body>
+</html>
